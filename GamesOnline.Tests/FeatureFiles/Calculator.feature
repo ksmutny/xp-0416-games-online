37f2534e7cf49d3c051b9d525d636eac27166cd4
Karel Smutný <karel.smutny@outlook.com>
2016-04-15 15:03:45 +0200

    added SpecFlow and a sample BDD test

---
+++ GamesOnline.Tests/FeatureFiles/Calculator.feature
@@ -0,0 +1,5 @@
+﻿Feature: Calculator
+
+Scenario: Add two numbers
+	When I add numbers 3 and 5
+	Then the result is 8
---
+++ GamesOnline.Tests/Models/CalculatorSteps.cs
@@ -0,0 +1,24 @@
+﻿using GamesOnline.Models;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace WebApplication1.Tests
+{
+    [Binding]
+    class CalculatorSteps
+    {
+        private int actualResult;
+
+        [When(@"I add numbers (\d+) and (\d+)")]
+        public void WhenIAddNumbers(int a, int b)
+        {
+            actualResult = Calculator.Add(a, b);
+        }
+
+        [Then(@"the result is (\d+)")]
+        public void ThenTheResultIs(int expectedResult)
+        {
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
