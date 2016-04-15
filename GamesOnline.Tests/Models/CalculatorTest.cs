504138a34194719d206d7b32ea5a43fce7891807
Karel Smutný <karel.smutny@outlook.com>
2016-04-15 14:49:53 +0200

    added xUnit and a sample test

---
+++ GamesOnline.Tests/Models/CalculatorTest.cs
@@ -0,0 +1,14 @@
+﻿using GamesOnline.Models;
+using Xunit;
+
+namespace GamesOnline.Tests.Models
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void Add()
+        {
+            Assert.Equal(5, Calculator.Add(2, 3));
+        }
+    }
+}
---
+++ GamesOnline/Models/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace GamesOnline.Models
+{
+    public class Calculator
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
