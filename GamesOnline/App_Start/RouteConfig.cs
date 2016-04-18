1ed1184de8e1951d8485b05a6220edaa86fc20d5
JanBN <jpivarcek@PD1.LOCAL>
2016-04-18 16:14:51 +0200

    p

--- GamesOnline/App_Start/RouteConfig.cs
+++ GamesOnline/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace GamesOnline
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Nim", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ GamesOnline/Controllers/NimController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GamesOnline.Controllers
+{
+    public class NimController : Controller
+    {
+        public ActionResult Index(string id)
+        {
+            int model = 0;
+            return View(model);
+        }
+
+
+        public ActionResult Move(string id, int pile, int count )
+        {
+            int model = 0;
+            return Json(model, JsonRequestBehavior.DenyGet);
+        }
+    }
+}
