d77da76ed78604e5d99fd54b7c0f4136c8c72dcc
Jan Prochazka <jan.prochazka@gmail.ccom>
2016-04-18 16:17:37 +0200

    gamestate

--- GamesOnline/Models/Repository.cs
+++ GamesOnline/Models/Repository.cs
@@ -37,7 +37,17 @@ namespace GamesOnline.Models
             if (gameid == null || !ActiveGames.ContainsKey(gameid)) return new GameState { ErrorMessage = "Game expired" };
             var state = ActiveGames[gameid];
             if (pile <= 0 || pile >= state.Piles.Length) return new GameState { ErrorMessage = "Invalid pile" };
-            if (count < 0 || count>state.Piles[pile]) return new GameState { ErrorMessage = "Invalid count" };
+            if (count < 0 || count > state.Piles[pile]) return new GameState { ErrorMessage = "Invalid count" };
+            state.Piles[pile] -= count;
+
+            if (state.Piles.All(x => x == 0))
+            {
+                state.PlayerWins = state.PlayerOnTheMove;
+            }
+            if (state.PlayerWins == 0)
+            {
+                state.PlayerOnTheMove = state.PlayerOnTheMove == 1 ? 2 : 1;
+            }
             return state;
         }
     }
