3caefb31b05b625b89f99e2650f40b325912ba6c
JanBN <jpivarcek@PC-PIVARCEK>
2016-04-20 12:13:20 +0200

    test

--- GamesOnline.Tests/FeatureFiles/NimAI.feature
+++ GamesOnline.Tests/FeatureFiles/NimAI.feature
@@ -1,7 +1,31 @@
 ﻿Feature: NimAI
 	
-Scenario: Add two numbers
-	Given I have entered 50 into the calculator
-	And I have entered 70 into the calculator
-	When I press add
-	Then the result should be 120 on the screen
+Scenario: ComputerWins
+       Given Piles And Computer is on the move
+       | 7 |
+       When Take all coins from the last pile
+          Then Computer wins
+
+Scenario: ComputerIsWinningBeforeMove
+       Given State of game before move is LOOSING
+          And Computer is on the move
+       When Computer makes move
+          Then after move state of game is WINNING
+And Player 1 is on the move
+
+
+Scenario: ComputerIsWinningEasyTest
+       Given Piles And Computer is on the move
+       | 1 | 2 | 
+       When Computer makes move
+          Then The piles are
+              | 1 | 1 |
+       And Player 1 is on the move
+
+Scenario: ComputerIsLoosingBeforeMove
+       Given State of game before move is WINNING
+          And Computer is on the move
+       When Computer makes move
+       Then after move state of game is LOOSING
+          And Player 1 is on the move
+          And Count of coins of any pile is less by 1  
---
+++ GamesOnline.Tests/NimAISteps.cs
@@ -0,0 +1,96 @@
+﻿using GamesOnline.Models;
+using System;
+using System.Linq;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace GamesOnline.Tests
+{
+    [Binding]
+    public class NimAISteps
+    {
+        private Nim _currentGame;
+
+        [Given(@"Piles And Computer is on the move")]
+        public void GivenPiles(Table table)
+        {
+            var piles = table.Rows[0].Values.ToList().Select(uint.Parse).ToArray();
+            var state = new Nim(Player.Two, piles.Select(x => (uint)x).ToArray());
+            this._currentGame = state;
+        }
+
+        [When(@"Take all coins from the last pile")]
+        public void WhenTakeAllCoinsFromTheLastPile()
+        {
+            uint lastPile = (uint)_currentGame.Piles.Count() - 1;
+            int coins = (int)_currentGame.Piles[lastPile];
+            _currentGame.TakeCoins(coins, lastPile);
+        }
+
+        [Then(@"Computer wins")]
+        public void ThenComputerWins()
+        {
+            var computerWins =_currentGame.IsOver && _currentGame.ActivePlayer == Player.Two;
+            Assert.Equal(true, computerWins);
+        }
+
+
+        //[Given(@"Computer is on the move")]
+        //public void GivenComputerIsOnTheMove()
+        //{
+
+
+        //    var piles = new uint[] { 5, 2, 5 };
+
+        //    var state = repo.NewAIGame("test")
+
+        //    Assert.Equal(piles, state.Piles);
+
+        //    ScenarioContext.Current.Pending();
+        //}
+        
+        //[Given(@"State of game before move is LOOSING")]
+        //public void GivenStateOfGameBeforeMoveIsLOOSING()
+        //{
+        //    ScenarioContext.Current.Pending();
+        //}
+        
+        //[Given(@"State of game before move is WINNING")]
+        //public void GivenStateOfGameBeforeMoveIsWINNING()
+        //{
+        //    ScenarioContext.Current.Pending();
+        //}
+        
+        //[When(@"Computer makes move")]
+        //public void WhenComputerMakesMove()
+        //{
+        //    ScenarioContext.Current.Pending();
+        //}
+        
+
+        
+        //[Then(@"after move state of game is WINNING")]
+        //public void ThenAfterMoveStateOfGameIsWINNING()
+        //{
+        //    ScenarioContext.Current.Pending();
+        //}
+        
+        //[Then(@"The piles are")]
+        //public void ThenThePilesAre(Table table)
+        //{
+        //    ScenarioContext.Current.Pending();
+        //}
+        
+        //[Then(@"after move state of game is LOOSING")]
+        //public void ThenAfterMoveStateOfGameIsLOOSING()
+        //{
+        //    ScenarioContext.Current.Pending();
+        //}
+        
+        //[Then(@"Count of coins of any pile is less by (.*)")]
+        //public void ThenCountOfCoinsOfAnyPileIsLessBy(int p0)
+        //{
+        //    ScenarioContext.Current.Pending();
+        //}
+    }
+}
