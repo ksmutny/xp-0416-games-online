4aa83f7ecb4a857042ab9bd34c0d680f1727fe78
jirimuller <jiri-muller@seznam.cz>
2016-04-20 10:22:30 +0200

    dsafsafada

--- GamesOnline.Tests/Minimax/MinimaxTest.cs
+++ GamesOnline.Tests/Minimax/MinimaxTest.cs
@@ -11,42 +11,42 @@ namespace GamesOnline.Tests.Minimax
 {
     public class MinimaxTest
     {
-        private double Evaluate(int state)
-        {
-            return state;
-        }
+        //private double Evaluate(int state)
+        //{
+        //    return state;
+        //}
 
-        private List<int> Generate(int state)
-        {
-            var result = new List<int>
-            {
-                state % 7,
-                state * 3 % 13,
-                state * 5 % 17
-            };
+        //private List<int> Generate(int state)
+        //{
+        //    var result = new List<int>
+        //    {
+        //        state % 7,
+        //        state * 3 % 13,
+        //        state * 5 % 17
+        //    };
 
-            return result;
-        }
+        //    return result;
+        //}
 
-        [Fact]
-        public void EvaluateTest_Simple()
-        {
-            var state = 5;
-            var minimax = new GamesOnline.AI.Minimax<int>(1, Evaluate, Generate);
-            var nextState = minimax.NextState(state);
+        //[Fact]
+        //public void EvaluateTest_Simple()
+        //{
+        //    var state = 5;
+        //    var minimax = new GamesOnline.AI.Minimax<int>(1, Evaluate, Generate);
+        //    var nextState = minimax.NextState(state);
 
-            Assert.Equal(8, nextState);
-        }
+        //    Assert.Equal(8, nextState);
+        //}
 
-        [Fact]
-        public void EvaluateTest_SecondLevel()
-        {
-            var state = 5;
-            var minimax = new GamesOnline.AI.Minimax<int>(2, Evaluate, Generate);
-            var nextState = minimax.NextState(state);
+        //[Fact]
+        //public void EvaluateTest_SecondLevel()
+        //{
+        //    var state = 5;
+        //    var minimax = new GamesOnline.AI.Minimax<int>(2, Evaluate, Generate);
+        //    var nextState = minimax.NextState(state);
 
-            Assert.Equal(5, nextState);
-        }
+        //    Assert.Equal(5, nextState);
+        //}
 
     }
 }
