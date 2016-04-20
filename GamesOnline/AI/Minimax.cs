4fd948364fcd93a10178dff90c8909432826de3a
jirimuller <jiri-muller@seznam.cz>
2016-04-20 10:20:01 +0200

    afsafas

--- GamesOnline/AI/Minimax.cs
+++ GamesOnline/AI/Minimax.cs
@@ -7,42 +7,42 @@ namespace GamesOnline.AI
 {
     public class Minimax<T>
     {
-        private Func<T, double> evaluate;
-        private Func<T, List<T>> generate;
-        private int depth;
+        //private Func<T, double> evaluate;
+        //private Func<T, List<T>> generate;
+        //private int depth;
 
-        public Minimax(int depth, Func<T, double> evaluate, Func<T, List<T>> generate)
-        {
-            this.depth = depth;
-            this.evaluate = evaluate;
-            this.generate = generate;
-        }
+        //public Minimax(int depth, Func<T, double> evaluate, Func<T, List<T>> generate)
+        //{
+        //    this.depth = depth;
+        //    this.evaluate = evaluate;
+        //    this.generate = generate;
+        //}
 
-        public T NextState(T state, int actDepth)
-        {
-            if (actDepth == 0)
-            {
+        //public T NextState(T state, int actDepth)
+        //{
+        //    if (actDepth == 0)
+        //    {
                 
-            }
+        //    }
 
-            var maxState = double.MinValue;
-            var states = this.generate(state);
-            foreach (var item in states)
-            {
-                NextState(item, actDepth - 1);
-            }
+        //    var maxState = double.MinValue;
+        //    var states = this.generate(state);
+        //    foreach (var item in states)
+        //    {
+        //        NextState(item, actDepth - 1);
+        //    }
 
 
-            //var values = states.Select(this.evaluate);
-            //var maxState = states.GroupBy(s => s, s => this.evaluate(s)).Max(s => s.Key); 
+        //    //var values = states.Select(this.evaluate);
+        //    //var maxState = states.GroupBy(s => s, s => this.evaluate(s)).Max(s => s.Key); 
             
 
 
-            var val = takeMax ? values.Max() : values.Min();
+        //    //var val = takeMax ? values.Max() : values.Min();
 
-            var result = states.First(x => this.evaluate(x) == val);
+        //    var result = states.First(x => this.evaluate(x) == val);
 
-            return result;
-        }
+        //    return result;
+        //}
     }
 }
